0dd820d7e0cbbb5c69b536880bfda6d841e87194
unknown <anhnt.tb12@gmail.com>
2022-02-11 14:12:26 +0700

    circle

---
+++ src/Circle.java
@@ -0,0 +1,20 @@
+public class Circle {
+    private double radius = 1;
+    private String color = "red";
+
+    public Circle() {
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+    public double getArea(){
+        double area =  Math.pow(radius,2)*Math.PI;
+        return area;
+    }
+}
+
